dd228018d0221e57915870df27971b107f5b6905
MikhailMaltsev <hack.the.life@gmail.com>
2020-08-06 03:16:06 +0700

    Hello, world!

---
+++ module_2/module_2_project.py
@@ -0,0 +1,421 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Введение
+
+# Нас (возможно и вас) пригласили поучаствовать в одном из проектов UNICEF — международного подразделения ООН, чья миссия состоит в повышении уровня благополучия детей по всему миру. 
+# 
+# Суть проекта — отследить влияние условий жизни учащихся в возрасте от 15 до 22 лет на их успеваемость по математике, чтобы на ранней стадии выявлять студентов, находящихся в группе риска.
+
+# In[867]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from itertools import combinations
+from scipy.stats import ttest_ind
+
+
+# # Ознакомление
+# Для начала загрузим файл в датафрейм (ДФ), переименуем некоторые колонки для удобства и посмотрим развернутый ДФ.
+
+# In[868]:
+
+
+students = pd.read_csv('stud_math.csv')
+students.rename(columns={'studytime, granular': 'studytime_granular',
+                         'Pstatus': 'pstatus', 'Medu': 'medu', 'Fedu': 'fedu',
+                         'Mjob': 'mjob', 'Fjob': 'fjob'}, inplace=True)
+pd.set_option('max_columns', None)
+
+
+# In[869]:
+
+
+students.sample(10)
+
+
+# #### Значения колонок датафрейма
+# 
+# school — аббревиатура школы, в которой учится ученик
+# 
+# sex — пол ученика ('F' - женский, 'M' - мужской)
+# 
+# age — возраст ученика (от 15 до 22)
+# 
+# address — тип адреса ученика ('U' - городской, 'R' - за городом)
+# 
+# famsize — размер семьи('LE3' <= 3, 'GT3' >3)
+# 
+# Pstatus — статус совместного жилья родителей ('T' - живут вместе 'A' - раздельно)
+# 
+# Medu — образование матери (0 - нет, 1 - 4 класса, 2 - 5-9 классы, 3 - среднее специальное или 11 классов, 4 - высшее)
+# 
+# Fedu — образование отца (0 - нет, 1 - 4 класса, 2 - 5-9 классы, 3 - среднее специальное или 11 классов, 4 - высшее)
+# 
+# Mjob — работа матери ('teacher' - учитель, 'health' - сфера здравоохранения, 'services' - гос служба, 'at_home' - не работает, 'other' - другое)
+# 
+# Fjob — работа отца ('teacher' - учитель, 'health' - сфера здравоохранения, 'services' - гос служба, 'at_home' - не работает, 'other' - другое)
+# 
+# reason — причина выбора школы ('home' - близость к дому, 'reputation' - репутация школы, 'course' - образовательная программа, 'other' - другое)
+# 
+# guardian — опекун ('mother' - мать, 'father' - отец, 'other' - другое)
+# 
+# traveltime — время в пути до школы (1 - <15 мин., 2 - 15-30 мин., 3 - 30-60 мин., 4 - >60 мин.)
+# 
+# studytime — время на учёбу помимо школы в неделю (1 - <2 часов, 2 - 2-5 часов, 3 - 5-10 часов, 4 - >10 часов)
+# 
+# failures — количество внеучебных неудач (n, если 1<=n<=3, иначе 0)
+# 
+# schoolsup — дополнительная образовательная поддержка (yes или no)
+# 
+# famsup — семейная образовательная поддержка (yes или no)
+# 
+# paid — дополнительные платные занятия по математике (yes или no)
+# 
+# activities — дополнительные внеучебные занятия (yes или no)
+# 
+# nursery — посещал детский сад (yes или no)
+# 
+# higher — хочет получить высшее образование (yes или no)
+# 
+# internet — наличие интернета дома (yes или no)
+# 
+# romantic — в романтических отношениях (yes или no)
+# 
+# famrel — семейные отношения (от 1 - очень плохо до 5 - очень хорошо)
+# 
+# freetime — свободное время после школы (от 1 - очень мало до 5 - очень мого)
+# 
+# goout — проведение времени с друзьями (от 1 - очень мало до 5 - очень много)
+# 
+# health — текущее состояние здоровья (от 1 - очень плохо до 5 - очень хорошо)
+# 
+# absences — количество пропущенных занятий
+# 
+# score — баллы по госэкзамену по математике
+
+# #### ДФ содержит информацию о 395 учащихся. Некоторые колонки содержат пустые значения. Посмотрим информацию о колонках.
+
+# In[870]:
+
+
+students.info()
+
+
+# # Очистка данных
+
+# In[871]:
+
+
+# Функция для получения быстрой справки о данных в колонках
+def info(x):
+    print(pd.DataFrame(x.value_counts()))
+    print('Пропущенных значений -', x.isnull().values.sum())
+
+
+# ### Просмотр числовыx столбцов
+
+# In[872]:
+
+
+info(students.age)
+
+
+# In[873]:
+
+
+info(students.medu)
+
+
+# In[874]:
+
+
+info(students.fedu)
+
+
+# Исправим опечатку значения 40.0, вероятно имелось ввиду 4.
+
+# In[875]:
+
+
+students.fedu = students.fedu.apply(lambda x: x/10 if x > 9 else x)
+
+
+# In[876]:
+
+
+info(students.traveltime)
+
+
+# In[877]:
+
+
+info(students.studytime)
+
+
+# In[878]:
+
+
+info(students.failures)
+
+
+# In[879]:
+
+
+info(students.studytime_granular)
+
+
+# In[880]:
+
+
+info(students.famrel)
+
+
+# Исправим опечатку значения -1.0, вероятно имелось ввиду 1.0
+
+# In[881]:
+
+
+students.famrel = students.famrel.apply(lambda x: abs(x) if x < 0 else x)
+
+
+# In[882]:
+
+
+info(students.freetime)
+
+
+# In[883]:
+
+
+info(students.goout)
+
+
+# In[884]:
+
+
+info(students.health)
+
+
+# In[885]:
+
+
+info(students.absences)
+
+
+# In[886]:
+
+
+info(students.score)
+
+
+# Числовые колонки содержат достаточно чистые данные, мало ошибок и пропущенных значений.
+
+# ### Просмотр строковых столбцов
+
+# In[887]:
+
+
+info(students.school)
+
+
+# In[888]:
+
+
+info(students.sex)
+
+
+# In[889]:
+
+
+info(students.address)
+
+
+# In[890]:
+
+
+info(students.famsize)
+
+
+# In[891]:
+
+
+info(students.pstatus)
+
+
+# In[892]:
+
+
+info(students.mjob)
+
+
+# In[893]:
+
+
+info(students.fjob)
+
+
+# In[894]:
+
+
+info(students.reason)
+
+
+# In[895]:
+
+
+info(students.guardian)
+
+
+# In[896]:
+
+
+info(students.schoolsup)
+
+
+# In[897]:
+
+
+info(students.famsup)
+
+
+# In[898]:
+
+
+info(students.paid)
+
+
+# In[899]:
+
+
+info(students.activities)
+
+
+# In[900]:
+
+
+info(students.nursery)
+
+
+# In[901]:
+
+
+info(students.higher)
+
+
+# In[902]:
+
+
+info(students.internet)
+
+
+# In[903]:
+
+
+info(students.romantic)
+
+
+# Строковые данные выглядят похуже числовых, но максимальное количество пропущенных значений в колонках лежит в пределах 10% от общего количества учащихся, что допустимо в рамках этой задачи, по моему мнению.
+
+# In[904]:
+
+
+# Можно было бы заменить все NaN на None, как обсуждалось в группе по проекту, но в таком случае перестает работать корреляция.
+# students = students.where(pd.notnull(students), None)
+
+
+# # Поиск зависимостей
+# Посмотрим корреляцию числовых значений
+
+# In[905]:
+
+
+students.corr()
+
+
+# Полная корреляция столбцов studytime и studytime_granular позволяют не брать последний в рассчет.
+# 
+# Образование родителей (medu и fedu) и кол-во внеучебных неудач (failures) больше других оказывают влияние на успеваемость. Т.е. чем выше образование родителей, тем более успешный ребенок в жизни в целом, в том числе и в учебе. 
+# 
+# Также можно увидеть корреляцию возраста (age) и кол-ва внеучебных неудач (failures). Чем страше ребенок, тем хуже успеваемость. Можно предположить, что более молодые родители имеют лучшее образование, либо растущие дети начинают больше времени уделять друзьям (gout) а не учебе (studytime).
+# 
+# Присутствует также логическая связь влияния пропусков занятий (absences) на успеваемость, эти данные мы тоже возьмем в модель.
+
+# Построим графики и посмотрим на распределения строковых данных
+
+# In[906]:
+
+
+def get_boxplot(column):
+    fig, ax = plt.subplots(figsize=(14, 4))
+    sns.boxplot(x=column, y='score',
+                data=students.loc[students.loc[:, column].isin(students.loc[:, column].value_counts().index[:10])], ax=ax)
+    plt.xticks(rotation=0)
+    ax.set_title('Boxplot for ' + column)
+    plt.show()
+
+
+# In[907]:
+
+
+for col in ['school', 'address', 'famsize', 'pstatus', 'mjob', 'fjob', 'reason', 'guardian', 'schoolsup', 'famsup', 'paid', 
+            'activities', 'nursery', 'higher', 'sex', 'internet', 'romantic']:
+    get_boxplot(col)
+
+
+# По условию задачи поиска группы риска, оцениваем факторы влияющие на ухудшение успеваемости.
+# Из графиков мы видим, что минимальных значений больше у учащихся:
+# <ul>
+#     <li>школы GP</li>
+#     <li>живущих за городом (address)</li>
+#     <li>с размеров семьи больше 3 человек (famsize)</li>
+#     <li>родители которых проживают совместно (pstatus)
+#     <li>опекуном которых является мать (guardian) - статистическое большинство
+#     <li>имеющих учебную поддержку дома (famsup)
+#     <li>не занимающихся дополнительно (платно) (paid)
+#     <li>посещавших детский сад
+#     <li>не собирающихся получать высшее образование (higher) - сильно влияет
+#     <li>имеющих доступ к интернету
+# </ul>
+
+# Сделаем тест Стюдента
+
+# In[908]:
+
+
+def get_stat_dif(column):
+    cols = students.loc[:, column].value_counts().index[:20]
+    combinations_all = list(combinations(cols, 2))
+    for comb in combinations_all:
+        if ttest_ind(students.loc[students.loc[:, column] == comb[0], 'score'],
+                     students.loc[students.loc[:, column] == comb[1], 'score']).pvalue \
+                <= 0.05/len(combinations_all):  # Учли поправку Бонферони
+            print('Найдены статистически значимые различия для колонки', column)
+            break
+
+
+# In[909]:
+
+
+for col in ['school', 'address', 'famsize', 'pstatus', 'mjob', 'fjob', 'reason', 'guardian', 'schoolsup', 'famsup', 'paid',
+            'activities', 'nursery', 'higher', 'sex', 'internet', 'romantic']:
+    get_stat_dif(col)
+
+
+# ### Собираем датасет для модели
+# Немного выводов:
+# <ol>
+#     <li>В целом, сильно влияет образование родителей. Они коррелируют достаточно сильно, но я решил не объединять, чтобы выявить будущие зависимости от пола родителей. Образование отца сильнее влияет на успеваемость, но также выделяется позиция матери преподавателя.</li>
+#     <li>Также сильно влияют на успеваемость планы учащегося поступать в высшее учебное заведение. Если таких планов нет - то и мотивация соответствующая.</li>
+#     <li>Остальные параметры в меньшей степени влияют на результат, но их сумма может оказать решающее воздействие.</li>
+# </ol>
+
+# In[915]:
+
+
+model = students[['school','age', 'address', 'medu', 'fedu', 'mjob', 'fjob', 'studytime', 'failures', 'higher', 'goout', 'absences', 'score']]
+model
+
